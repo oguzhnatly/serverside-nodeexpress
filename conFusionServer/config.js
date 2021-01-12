caeee5e2cdb7d4dfaeb208ece236063dcdaf80b0
Oğuzhan Atalay <54781138+oguzhnatly@users.noreply.github.com>
2021-01-12 11:39:28 +0300

    config.js updated

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'secretKey': 'SECRET_KEY',
+    'mongoUrl': 'mongodb://localhost:27017/MONGODB_NAME',
     'facebook': {
-        clientId: '806386566857403',
-        clientSecret: 'c96bce345b9a49415461244a8afcb7e0'
+        clientId: 'FACEBOOK_CLIENT_ID',
+        clientSecret: 'FACEBOOK_CLIENT_SECRET'
     }
 }
